e4a234a8c33ab54f88e454e9964e09b468e31a68
Ratnesh Varma <ratneshvarma@ntuclink.com.sg>
2022-01-09 14:34:06 +0800

    added prometheus and grafana
    added prometheus and grafana

--- elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/config/WebClientConfig.java
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/config/WebClientConfig.java
@@ -7,7 +7,6 @@ import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
